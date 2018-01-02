34336129f9d4e8dfef39cdaa395fe15b75981ebd
Feideus <erwan.ulrich@gmail.com>
2018-01-02 15:16:51 +0100

    first commit

--- jsau-webapp/src/component/bar.js
+++
@@ -1,7 +0,0 @@
-export default {
-    data() {
-        return {
-            msg: 'This is Babar'
-        }
-    }
-}
--- jsau-webapp/src/component/bar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h2>Bar</h2>
-    {{ msg }}
-  </div>
-</template>
-
-<script src="./bar.js"></script>
